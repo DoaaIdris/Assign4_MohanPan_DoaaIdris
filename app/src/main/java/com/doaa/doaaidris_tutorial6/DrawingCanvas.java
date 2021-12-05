91cc8afdbccfb4c9bbd527fe4b4a77139d966285
Doaa <doaa.id00@gmail.com>
2021-12-04 23:10:04 -0500

    Added multi drawing, undo, clear, erasor, color picker, and line

--- app/src/main/java/com/doaa/doaaidris_tutorial6/DrawingCanvas.java
+++ app/src/main/java/com/doaa/doaaidris_tutorial6/DrawingCanvas.java
@@ -16,13 +16,18 @@ import java.util.LinkedList;
 public class DrawingCanvas extends View {
 
     private Paint mPaint;
-    private Path mPath;
+    public Path mPath;
     LinkedList<Paint> paintContainer;
-    LinkedList<Path> pathsContainer;
+    public LinkedList<Path> pathsContainer;
 
     public int pathColour;
-
     public int pathWidth;
+
+    float radius;
+    float x;
+    float y;
+
+    public int mode = 0;
 
     public DrawingCanvas(Context context, AttributeSet attrs){
         super(context, attrs);
@@ -36,30 +41,90 @@ public class DrawingCanvas extends View {
 
     @Override
     protected void onDraw(Canvas canvas) {
+
         if(pathsContainer.size() > 0){
             for(int i = 0; i < pathsContainer.size(); i++) {
                 canvas.drawPath(pathsContainer.get(i), paintContainer.get(i));
+                pathsContainer.getFirst().addCircle(x, y, radius, Path.Direction.CW);
                 super.onDraw(canvas);
             }
+
+        }
+
+    }
+
+    public void clearCanvas(){
+        pathsContainer.clear();
+        paintContainer.clear();
+        invalidate();
+    }
+
+    public void undoCanvas(){
+        if (pathsContainer.size() > 0){
+            pathsContainer.removeFirst();
+            paintContainer.removeFirst();
+            invalidate();
         }
     }
+
 
     @Override
     public boolean onTouchEvent(MotionEvent event) {
 
         int touchCount = event.getPointerCount();
-
         float pressure = event.getPressure();
-       // System.out.println(pressure);
+        // System.out.println(pressure);
+        int index = event.getActionIndex();
+        int id = event.getPointerId(index);
 
-        if(touchCount == 1){
-            switch (event.getAction()){
+        if(mode == 0) {
+            switch (event.getActionMasked()){
                 case MotionEvent.ACTION_DOWN:
+
+                case MotionEvent.ACTION_POINTER_DOWN:
+                    mPath = new Path();
+                    mPaint = new Paint();
+
                     mPaint.setColor(pathColour);
                     mPaint.setStyle(Paint.Style.STROKE);
                     mPaint.setStrokeJoin(Paint.Join.ROUND);
                     mPaint.setStrokeCap(Paint.Cap.ROUND);
+                    mPaint.setStrokeWidth(pressure * pathWidth);
 
+                    mPath.moveTo(event.getX(index), event.getY(index));
+                    pathsContainer.add(id, mPath);
+                    paintContainer.add(id, mPaint);
+
+                    break;
+
+                case MotionEvent.ACTION_MOVE:
+                    for(int i = 0; i < event.getPointerCount(); i++){
+                        id = event.getPointerId(i);
+                        mPath = pathsContainer.get(id);
+                        mPath.lineTo(event.getX(i), event.getY(i));
+                    }
+
+                    invalidate();
+                    break;
+
+                case MotionEvent.ACTION_UP:
+                case MotionEvent.ACTION_POINTER_UP:
+                    mPath = pathsContainer.get(id);
+                    break;
+
+            }
+        }
+
+        else if (mode == 1){
+            switch (event.getActionMasked()){
+                case MotionEvent.ACTION_DOWN:
+                    mPath = new Path();
+                    mPaint = new Paint();
+
+                    mPaint.setColor(Color.parseColor("#FFFFFF"));
+                    mPaint.setStyle(Paint.Style.STROKE);
+                    mPaint.setStrokeJoin(Paint.Join.ROUND);
+                    mPaint.setStrokeCap(Paint.Cap.ROUND);
                     mPaint.setStrokeWidth(pressure * pathWidth);
 
                     pathsContainer.addLast(mPath);
@@ -70,79 +135,95 @@ public class DrawingCanvas extends View {
                     break;
 
                 case MotionEvent.ACTION_MOVE:
+
                     pathsContainer.getLast().lineTo(event.getX(), event.getY());
                     invalidate();
                     break;
 
                 case MotionEvent.ACTION_UP:
-                    mPaint = new Paint();
+
+                    break;
+
+            }
+        }
+
+        else if (mode == 2){
+            switch (event.getAction()){
+                case MotionEvent.ACTION_DOWN:
+
+                    mPaint.setColor(pathColour);
+                    mPaint.setStyle(Paint.Style.STROKE);
+                    mPaint.setStrokeJoin(Paint.Join.ROUND);
+                    mPaint.setStrokeCap(Paint.Cap.ROUND);
+                    mPaint.setStrokeWidth(pressure * pathWidth);
+
+                    pathsContainer.addFirst(mPath);
+                    paintContainer.addFirst(mPaint);
+
+                    pathsContainer.getFirst().moveTo(event.getX(), event.getY());
+
+                    break;
+
+                case MotionEvent.ACTION_MOVE:
+
+                    break;
+
+                case MotionEvent.ACTION_UP:
+
                     mPath = new Path();
+                    mPaint = new Paint();
 
-                    break;
+                    pathsContainer.getLast().lineTo(event.getX(), event.getY());
 
-
-            }
-        }
-        else if (touchCount == 2){
-            switch (event.getAction()){
-                case MotionEvent.ACTION_DOWN:
-                    mPaint.setColor(pathColour);
-                    mPaint.setStyle(Paint.Style.STROKE);
-                    mPaint.setStrokeJoin(Paint.Join.ROUND);
-                    mPaint.setStrokeCap(Paint.Cap.ROUND);
-
-                    mPaint.setStrokeWidth(pressure * pathWidth);
-
-                    pathsContainer.addLast(mPath);
-                    paintContainer.addLast(mPaint);
-
-                    pathsContainer.getLast().moveTo(event.getX(0), event.getY(0));
-                    pathsContainer.getLast().lineTo(event.getX(1), event.getY(1));
-
-                    break;
-
-                case MotionEvent.ACTION_MOVE:
-                    pathsContainer.getLast().reset();
-                    pathsContainer.getLast().moveTo(event.getX(0), event.getY(0));
-                    pathsContainer.getLast().lineTo(event.getX(1), event.getY(1));
                     invalidate();
-                    break;
 
-            }
-        }
-        else if (touchCount == 3){
-            switch (event.getAction()){
-                case MotionEvent.ACTION_DOWN:
-                    mPaint.setColor(pathColour);
-                    mPaint.setStyle(Paint.Style.STROKE);
-                    mPaint.setStrokeJoin(Paint.Join.ROUND);
-                    mPaint.setStrokeCap(Paint.Cap.ROUND);
-
-                    mPaint.setStrokeWidth(pressure * pathWidth);
-
-                    pathsContainer.addLast(mPath);
-                    paintContainer.addLast(mPaint);
-
-                    pathsContainer.getLast().moveTo(event.getX(0), event.getY(0));
-                    pathsContainer.getLast().lineTo(event.getX(1), event.getY(1));
-                    pathsContainer.getLast().lineTo(event.getX(2), event.getY(2));
-                    pathsContainer.getLast().lineTo(event.getX(0), event.getY(0));
-
-                    break;
-
-                case MotionEvent.ACTION_MOVE:
-                    pathsContainer.getLast().reset();
-                    pathsContainer.getLast().moveTo(event.getX(0), event.getY(0));
-                    pathsContainer.getLast().lineTo(event.getX(1), event.getY(1));
-                    pathsContainer.getLast().lineTo(event.getX(2), event.getY(2));
-                    pathsContainer.getLast().lineTo(event.getX(0), event.getY(0));
-                    invalidate();
                     break;
 
             }
         }
 
 
+//        if (touchCount == 2){
+//            switch (event.getAction()){
+//                case MotionEvent.ACTION_DOWN:
+//                    mPaint = new Paint();
+//                    mPath = new Path();
+//
+//                    mPaint.setColor(pathColour);
+//                    mPaint.setStyle(Paint.Style.STROKE);
+//                    mPaint.setStrokeJoin(Paint.Join.ROUND);
+//                    mPaint.setStrokeCap(Paint.Cap.ROUND);
+//                    mPaint.setStrokeWidth(pressure * pathWidth);
+//
+//                    x = event.getX(0);
+//                    y = event.getY(0);
+//                    radius = (float) Math.hypot(event.getX(0) - event.getX(1), event.getY(0) - event.getY(1));
+//
+//                    //pathsContainer.addFirst(mPath);
+//                   // paintContainer.addFirst(mPaint);
+//
+//                    //pathsContainer.getFirst().addCircle(x, y, radius, Path.Direction.CW);
+//                    invalidate();
+//
+//                    break;
+//
+//                case MotionEvent.ACTION_MOVE:
+//                    //pathsContainer.getLast().reset();
+//
+//                    x = event.getX(0);
+//                    y = event.getY(0);
+//                    radius = (float) Math.hypot(event.getX(0) - event.getX(1), event.getY(0) - event.getY(1));
+//
+//
+//                    break;
+//
+//                case MotionEvent.ACTION_UP:
+//
+//                    break;
+//
+//
+//            }
+        //}
 
         return true;
     }
--- app/src/main/java/com/doaa/doaaidris_tutorial6/MainActivity.java
+++ app/src/main/java/com/doaa/doaaidris_tutorial6/MainActivity.java
@@ -2,12 +2,21 @@ package com.doaa.doaaidris_tutorial6;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.content.DialogInterface;
 import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
+import android.widget.Button;
 import android.widget.SeekBar;
 import android.widget.Spinner;
+import android.widget.Toast;
+
+import com.flask.colorpicker.ColorPickerView;
+import com.flask.colorpicker.OnColorSelectedListener;
+import com.flask.colorpicker.builder.ColorPickerClickListener;
+import com.flask.colorpicker.builder.ColorPickerDialogBuilder;
 
 import static android.graphics.Color.rgb;
 
@@ -15,7 +24,13 @@ public class MainActivity extends AppCompatActivity {
 
     DrawingCanvas dCanvas;
     SeekBar strokeWidth;
-    Spinner colourDropdown;
+
+    Button clearButton;
+    Button undoButton;
+    Button eraserButton;
+    Button lineButton;
+
+    View colorPicker;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,7 +40,13 @@ public class MainActivity extends AppCompatActivity {
         dCanvas = findViewById(R.id.myCanvas);
 
         strokeWidth = findViewById(R.id.seekBar);
-        colourDropdown = findViewById(R.id.spinner);
+
+        clearButton = findViewById(R.id.clearBtn);
+        undoButton = findViewById(R.id.undoBtn);
+        eraserButton = findViewById(R.id.eraserBtn);
+        lineButton = findViewById(R.id.lineBtn);
+
+        colorPicker = findViewById(R.id.colorPicker);
 
         strokeWidth.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
             @Override
@@ -49,17 +70,67 @@ public class MainActivity extends AppCompatActivity {
 
         });
 
-        colourDropdown.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+        colorPicker.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
-                String colour = colourDropdown.getSelectedItem().toString();
-                dCanvas.pathColour = Color.parseColor(colour);
-                System.out.println(colour);
+            public void onClick(View view) {
+                final Context context = MainActivity.this;
+                ColorPickerDialogBuilder
+                        .with(context)
+                        .setTitle("Choose color")
+                        .initialColor(Color.parseColor("#FFFFFF"))
+                        .wheelType(ColorPickerView.WHEEL_TYPE.FLOWER)
+                        .density(12)
+                        .setOnColorSelectedListener(new OnColorSelectedListener() {
+                            @Override
+                            public void onColorSelected(int selectedColor) {
+                                Toast.makeText(MainActivity.this,"onColorSelected: 0x" + Integer.toHexString(selectedColor),Toast.LENGTH_SHORT).show();
+                                colorPicker.setBackgroundColor(selectedColor);
+
+                            }
+                        })
+                        .setPositiveButton("ok", new ColorPickerClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int selectedColor, Integer[] allColors) {
+                                colorPicker.setBackgroundColor(selectedColor);
+                                dCanvas.pathColour = selectedColor ;
+                                dCanvas.mode = 0;
+                            }
+                        })
+                        .setNegativeButton("cancel", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+                            }
+                        })
+                        .build()
+                        .show();
             }
+        });
 
+        eraserButton.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onNothingSelected(AdapterView<?> adapterView) {
+            public void onClick(View view) {
+                dCanvas.mode = 1;
+            }
+        });
 
+        clearButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                dCanvas.clearCanvas();
+            }
+        });
+
+        undoButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                dCanvas.undoCanvas();
+            }
+        });
+
+        lineButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                dCanvas.mode = 2;
             }
         });
 
